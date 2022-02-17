21f20a38997321278ec802d5d62ade330c225243
williamnie <yebeixiaohai@gmail.com>
2022-02-17 11:12:41 +0800

    delete cache

--- src/renderer/.umi-production/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/xiaobei/Documents/private/toolbox/netSend/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','getInitialState','initialStateConfig','locale','locale','request',],
-});
-
-export { plugin };
--- src/renderer/.umi-production/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from '/Users/xiaobei/Documents/private/toolbox/netSend/node_modules/react-helmet';
