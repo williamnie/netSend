3a903131ea1c8a6b9494165e3625816ed6640994
williamnie <yebeixiaohai@gmail.com>
2022-03-04 18:35:00 +0800

    fix ubuntu build 失败：大小写问题

--- src/renderer/components/FileList/index.js
+++ src/renderer/components/FileList/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: xiaobei
  * @Date: 2021-02-04 16:06:03
- * @LastEditTime: 2022-03-04 17:32:48
+ * @LastEditTime: 2022-03-04 18:34:22
  * @LastEditors: xiaobei
  */
 import React, { useState, useImperativeHandle, forwardRef, } from 'react';
@@ -9,7 +9,7 @@ import { ImportOutlined } from '@ant-design/icons';
 import classnames from 'classnames';
 import { request } from 'umi';
 import { apiConfig } from '@/utils/config';
-import FileItem from '../FileItem/index'
+import FileItem from '../FileItems'
 import styles from './index.less';
 const FileList = (props, ref) => {
     const [visible, setVisible] = useState(false);
